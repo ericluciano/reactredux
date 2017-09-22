511d28f6ec03b286956ede5ce23b13b9eb9cfc30
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2017-09-22 09:49:46 -0300

    17.09.22 - correção no reducer

--- src/test1/test1Reducer.js
+++ src/test1/test1Reducer.js
@@ -3,11 +3,10 @@ const INITIAL_STATE = {
 }
 
 export default (state = INITIAL_STATE, action) => {
-	let i = 1;
 	switch(action.type){
 		case 'TEST1_ACTION':
 			return { ...state, list: [...state.list, {id: Math.random(), text: action.payload}], value: action.payload}
-			
+
 		default:
 			return state
 	}
