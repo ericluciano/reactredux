d0145b1e8e87cc7d10211fc1f183798270155116
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2017-09-27 17:28:08 -0300

    17.09.27 - add novas mensagens

---
+++ src/msg/msg.js
@@ -0,0 +1,6 @@
+export const Messages = {
+  //validação de token
+  token_invalid: 'Token inválido / Faça Login.',
+  token_not_provided: 'Token não especificado. JWT Bearer.',
+  logout: 'Até logo.',
+}
