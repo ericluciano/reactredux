f5a66a16ed9921959a86c0527ffc49e2ad4fb13d
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2017-09-27 10:23:19 -0300

    17.09.27 - arquivo de constantes add

---
+++ src/constantes/const.js
@@ -0,0 +1,7 @@
+export const ROOT_URL = 'http://mixriofm.uol.com.br'
+export const URL_BASE = `${ROOT_URL}/api`
+export const URL_PROMOCAO = `${URL_BASE}/promocoes`
+export const URL_LOGIN = `${URL_BASE}/login`
+export const URL_VALIDATE_TOKEN = `${URL_BASE}/validateToken`
+export const URL_UPLOADS = `${ROOT_URL}/uploads`
+export const URL_LOADING = `${URL_UPLOADS}/loading.gif`
