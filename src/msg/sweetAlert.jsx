f1233ac3a27f0ce04094cd35689c97926175aeb9
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2017-09-27 18:41:46 -0300

    17.09.27 - add SweetAlert

---
+++ src/msg/sweetAlert.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReduxSweetAlert from 'react-redux-sweetalert'
+import 'modules/sweetalert/dist/sweetalert.css';
+
+export default (props) => (
+  <ReduxSweetAlert />
+)
